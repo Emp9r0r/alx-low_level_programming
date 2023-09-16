e5c5e4cb34da242e6b47c9ff1c1e6a0cc6fff1c1
Emp9r0r <adekunleedward6@gmail.com>
2023-09-15 18:06:13 -0700

    print most number

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,18 +11,13 @@
 void print_most_numbers(void)
 
 {
-	int x = 0;
+	char c;
 
-	for (; x <= 9; x++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (x == 2 || x == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(x + '0');
-		}
-		_putchar('\n');
+		if (!(c == '2' || c == 4))
+			_putchar(x + '0')
 	}
+	_putchar('\n');
 }
+
