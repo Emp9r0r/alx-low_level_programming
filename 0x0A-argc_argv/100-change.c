7e64dc5c3da6507e288a325fd9695f506180bad4
Emp9r0r <edwardadekunle6@gmail.com>
2023-10-02 12:23:15 -0700

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,75 +1,40 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
+#include "main.h"
 
 /**
- * coinConverter - Calculates the minimum number of coins required for a given amount.
- * @k: The amount for which to calculate the minimum number of coins.
- *
- * Return: The minimum number of coins needed for the given amount.
- */
-
-int coinConverter(int k)
-{
-	int count = 0;
-
-	while (k != 0)
-	{
-		if (k % 10 == 9 || k % 10 == 7)
-			k -= 2;
-		else if (k % 25 == 0)
-			k -= 25;
-		else if (k % 10 == 0)
-			k -= 10;
-		else if (k % 5 == 0)
-			k -= 5;
-		else if (k % 2 == 0)
-		{
-			if (k % 10 == 6)
-				k -= 1;
-			else
-				k -= 2;
-		}
-		else
-			k -= 1;
-
-		count++;
-	}
-
-	return (count);
-}
-
-/**
- * main - Calculates the minimum number of coins needed for a given amount.
- * @argc: Number of command line arguments (should be 2: program name and amount)
+ * main - Calculates and prints the minimum number of coins.
+ * @argc: Number of command line arguments
  * @argv: Array of command line argument strings
- * Return: 0 if exactly 1 argument (excluding program name) is passed, 1 otherwise
+ *
+ * Return: 0 on success, 1 on error
  */
 
 int main(int argc, char *argv[])
 {
-	int k, coin;
-
-	coin = 0;
+	int num, l, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	k = atoi(argv[1]);
-
-	if (k < 0)
-		printf("0\n");
-	else
+	num = atoi(argv[1]);
+	result = 0;
+	if (num < 0)
 	{
-		coin = coinConverter(k);
-
-		printf("%d\n", coin);
+		printf("0\n");
+		return (0);
 	}
-
+	for (l = 0; l < 5 && num >= 0; l++)
+	{
+		while (num >= coins[l])
+		{
+			result++;
+			num -= coins[l];
+		}
+	}
+	printf("%d\n", result);
 	return (0);
 }
