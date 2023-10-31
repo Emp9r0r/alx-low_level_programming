99806dc648c387fc142fb4e0c7d247aa4ab248c6
Emp9r0r <edwardadekunle6@gmail.com>
2023-10-30 18:53:35 -0700

    make let Blessing rain

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int _strlen(char *str);
+char *create_buffer(char *file);
+void close_file(int fd);
+int _putchar(char c);
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
